a4696baf09c537c8a9d93041d35242e9a215280a
abhay67-cryto <abhayupadhyay7266@gmail.com>
2025-05-11 22:01:00 +0530

    Create script.js

---
+++ frontend/script.js
@@ -0,0 +1,12 @@
+const plane = document.getElementById("plane");
+const multiplierDisplay = document.getElementById("multiplier");
+const startBtn = document.getElementById("startBtn");
+const cashOutBtn = document.getElementById("cashOutBtn");
+const result = document.getElementById("result");
+const betAmountInput = document.getElementById("betAmount");
+
+let multiplier = 1.0;
+let interval;
+let crashed = false;
+let crashPoint = 
+￼Enter
